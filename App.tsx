74046ccbc22bdd7e8a637ca989bfc87f835bb73b
artur <g.infosecurity@gmail.com>
2021-10-08 00:32:50 +0300

    React-Navigation Broken

--- App.tsx
+++ App.tsx
@@ -3,18 +3,20 @@ import React from 'react';
 import LoginScreen from "./conponents/LoginScreen";
 import RecoverScreen from "./conponents/RecoverScreen";
 import ProfileScreen from "./conponents/ProfileScreen";
-import {View} from "react-native";
 
-import styles from "./Global.style";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
+const MainStack = createNativeStackNavigator();
 
 export default function App() {
 
     return (
-        <View style={styles.container}>
-            {/*<LoginScreen/>*/}
-            {/*<RecoverScreen/>*/}
-            <ProfileScreen/>
-        </View>
+        // <LoginScreen/>
+        <NavigationContainer>
+            <MainStack.Screen name="LoginScreen" component={LoginScreen}/>
+            <MainStack.Screen name="RecoverScreen" component={RecoverScreen}/>
+            <MainStack.Screen name="ProfileScreen" component={ProfileScreen}/>
+        </NavigationContainer>
     );
 };
